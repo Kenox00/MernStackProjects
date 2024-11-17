d315ebd087bbf9d9649862615a8e77c02b7e3244
Kenox00 <kwihanganaenock99@gmail.com>
2024-11-17 22:13:39 +0200

    signun and login form controllers

--- TaskManager/backend/model/userModel.js
+++ TaskManager/backend/model/userModel.js
@@ -1,6 +1,7 @@
 const  mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const validotor = require("validator");
 
 
 const userSchema = new Schema({
@@ -15,4 +16,54 @@ const userSchema = new Schema({
     }
 }, {timestamps: true})
 
-module.export  = mongoose.model("User", userSchema)
+
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if(!validotor.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if(!validotor.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
+
+    const exists = await this.findOne({ email })
+  
+    if (exists) {
+      throw Error('Email already in use')
+    }
+  
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+  
+    const user = await this.create({ email, password: hash })
+  
+    return user
+  }
+
+  // static signup method
+
+  userSchema.statics.login = async function(email, password) {
+
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+      throw Error('Incorrect email')
+    }
+  
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      throw Error('Incorrect password')
+    }
+  
+    return user
+  }
+module.exports = mongoose.model("User", userSchema)
